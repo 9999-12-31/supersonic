f709eaff978fe13ba0f1b8e317b43f7f9ef48df4
Ethan Mei (梅景治) <meijingzhi@inspur.com>
2024-07-31 18:27:26 +0800

    解决system/index冲突

--- webapp/packages/supersonic-fe/src/pages/System/index.tsx
+++ webapp/packages/supersonic-fe/src/pages/System/index.tsx
@@ -211,60 +211,64 @@ const System: React.FC = () => {
 
   return (
     <>
-      <div style={{ margin: '40px auto', width: 1200 }}>
-        <Row>
-          <Col span={18}>
-            <ProCard
-              title="系统设置"
-              extra={
-                <Space>
-                  <Button
-                    type="primary"
-                    onClick={() => {
-                      querySaveSystemConfig();
-                    }}
-                  >
-                    保 存
-                  </Button>
-                </Space>
-              }
-            >
-              <Form form={form}
-                    layout="vertical"
-                    className={styles.form}
-                    onValuesChange={(value, values) => {
-                      const valueKey = Object.keys(value)[0];
-                      excuteDepConfig(valueKey, values);
-                    }}>
-                <FormItem name="admins" label="管理员">
-                  <SelectTMEPerson placeholder="请邀请团队成员" />
-                </FormItem>
-                <Divider />
-                <Space direction="vertical" style={{ width: '100%' }} size={35}>
-                  {Object.keys(systemConfig).map((key: string) => {
-                    const itemList = systemConfig[key];
-                    return (
-                      <ProCard
-                        title={<span style={{ color: '#296df3' }}>{key}</span>}
-                        key={key}
-                        bordered
-                        id={key}
+       <div style={{ margin: '0 auto' }}>
+                <div style={{ background: '#fff', width: '15%', height: '1000px', position: 'fixed', boxShadow: '1px 1px 5px grey'}}>
+                  <div style={{ marginTop: 20 }}>
+                    <Anchor items={anchorItems} style={{ marginTop: 10 }}/>
+                  </div>
+                </div>
+
+                <div style={{ width: '84%', float: 'right' }}>
+
+                  <div style={{background: '#fff', height: '20vh', marginBottom: '10px'}}>
+                    <Form form={form} layout="vertical" className={styles.form} style={{padding:'20px'}}>
+                      <h2 style={{display:'inline-block'}}>系统设置</h2>
+                      <Button
+                        type="primary"
+                        onClick={() => {
+                          querySaveSystemConfig();
+                        }}
+                        style={{ float: 'right' }}
                       >
-                        {genneratorFormItemList(itemList, form)}
-                      </ProCard>
-                    );
-                  })}
-                </Space>
-              </Form>
-            </ProCard>
-          </Col>
-          <Col span={6} style={{ background: '#fff' }}>
-            <div style={{ marginTop: 20 }}>
-              <Anchor items={anchorItems} />
+                        保 存
+                      </Button>
+                      <FormItem name="admins" label="管理员">
+                        <SelectTMEPerson placeholder="请邀请团队成员"/>
+                      </FormItem>
+                    </Form>
+                  </div>
+
+                  <div style={{background: '#fff', height: '70vh', overflow:'auto'}}>
+                    <ProCard
+                      title=""
+                      extra={
+                        <Space>
+                        </Space>
+                      }
+                    >
+                      <Form form={form} layout="vertical" className={styles.form}>
+                        <Space direction="vertical" style={{ width: '100%' }} size={35}>
+                          {Object.keys(systemConfig).map((key: string) => {
+                            const itemList = systemConfig[key];
+                            return (
+                              <ProCard
+                                title={<span style={{ color: '#296df3' }}>{key}</span>}
+                                key={key}
+                                bordered
+                                id={key}
+                              >
+                                {genneratorFormItemList(itemList, form)}
+                              </ProCard>
+                            );
+                          })}
+                        </Space>
+                      </Form>
+                    </ProCard>
+                  </div>
+                </div>
+
             </div>
-          </Col>
-        </Row>
-      </div>
+
     </>
   );
 };
